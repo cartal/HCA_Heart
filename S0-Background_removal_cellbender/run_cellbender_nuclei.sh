2435240cef9a87fa448a5e196802f92ddb2ec3b2
cartal <cntalaveralopez@icloud.com>
2019-12-08 13:31:32 +0000

    Fixed for loop in S0

--- S0-Background_removal_cellbender/run_cellbender_nuclei.sh
+++ S0-Background_removal_cellbender/run_cellbender_nuclei.sh
@@ -8,14 +8,20 @@ OUTPUT_DIR=/mnt/data/hca_heart/cellbender/harvard/premrna/
 REPORTS=/mnt/data/hca_heart/cellranger/harvard/premrna/reports/
 
 ### RUN CELLBENDER
+# For this to work, you will need a file with at least three columns:
+#sampleID,expected_cells,included_cells
 
-for i in $(cat samples.txt)
+
+for i in $(echo $(cat samples.txt) | cut -f1 -d-)
 
 do
 
+    EXPECTED=$(echo $(cat samples.txt) | cut -f2 -d-)
+    INCLUDED=$(echo $(cat samples.txt) | cut -f3 -d-)
+
     ln -s $INPUT_DIR/$i/outs/$i.h5
     
-    cellbender remove-background --input $i.h5 --output $i.cellbender.h5 --expected-cells <INSERT> --total-droplets-included <INSERT> --epochs $E --cuda
+    cellbender remove-background --input $i.h5 --output $i.cellbender.h5 --expected-cells $EXPECTED --total-droplets-included $INCLUDED --epochs $E --cuda
 
     rm $i.h5
 
